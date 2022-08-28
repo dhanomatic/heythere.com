aa6f0f43cb6830d9a9081422111b1af864d69f55
Dhananjayan <dhananjayan.online@outlook.com>
2022-08-28 16:31:58 +0530

    staticfiles updated 6

---
+++ heythere/staticfiles/js/image-preview.48fbfbdcbe81.js
@@ -0,0 +1,25 @@
+const inFile = document.getElementById("inpFile");
+    const previewContainer = document.getElementById("imagePreview");
+    const previewImage = previewContainer.querySelector(".image-preview__image");
+    const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+    inpFile.addEventListener("change", function() {
+        const file = this.files[0];
+
+        if (file){
+            const reader = new FileReader();
+
+            previewDefaultText.style.display = "none";
+            previewImage.style.display = "block";
+
+            reader.addEventListener("load", function(){
+                previewImage.setAttribute("src", this.result);
+            });
+
+            reader.readAsDataURL(file);
+        }else{
+            previewDefaultText.style.display = null;
+            previewImage.style.display = null;
+            previewImage.setAttribute("src", "");
+        }
+    });
